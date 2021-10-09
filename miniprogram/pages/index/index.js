c06267b06c0131328d53c6bda6992bd02764e6fb
lojze.wang <lojze.wang@ly.com>
2021-10-09 13:40:41 +0800

    xiug

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -16,7 +16,7 @@ Page({
       success: function(res) {
         let list = res.result.ideasaiList?.data
 
-        console.log(list[list.length - 1].data[3])
+        console.log(list[list.length - 1])
         that.setData({
           now_time: list[list.length - 1].now_time,
           list: list[list.length - 1].data[3]
